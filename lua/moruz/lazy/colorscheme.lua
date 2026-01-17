87f48fb5ee20c7abbab654bb2ff4da6513de9cff
Carlos Peña <carlos_pena@outlook.de>
2026-01-17 10:18:24 -0600

    Feature: Transparent background added to the schema

--- lua/moruz/lazy/colorscheme.lua
+++ lua/moruz/lazy/colorscheme.lua
@@ -1,18 +1,17 @@
-
 return {
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("kanagawa").setup({
-    colors = {
-        palette = {
-            -- change all usages of these colors
-            sumiInk3 = "#1F1F28",
-        },
-    },
-        })
-        vim.cmd("colorscheme kanagawa")
-    end,
+	"rebelot/kanagawa.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("kanagawa").setup({
+			transparent = true,
+			colors = {
+				palette = {
+					-- change all usages of these colors
+					sumiInk3 = "#1F1F28",
+				},
+			},
+		})
+		vim.cmd("colorscheme kanagawa")
+	end,
 }
-
