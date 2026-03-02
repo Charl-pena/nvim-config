66085e6aa43dad2fe620b1bfbaaa84ca85667411
Carlos Peña <carlos_pena@outlook.de>
2026-03-01 21:01:21 -0600

    Maintenance: better options for cololizer

--- lua/moruz/lazy/cololizer.lua
+++ lua/moruz/lazy/cololizer.lua
@@ -1,6 +1,13 @@
 return {
-	"norcalli/nvim-colorizer.lua",
+	"NvChad/nvim-colorizer.lua",
+	event = "BufReadPre",
 	config = function()
-		require("colorizer").setup({ "*" })
+		require("colorizer").setup({
+			user_default_options = {
+				RGB = true,
+				RRGGBB = true,
+				names = false,
+			},
+		})
 	end,
 }
