effdfa1377d5be608dc66dfc700a3343fcff605a
Carlos Peña <carlos_pena@outlook.de>
2026-03-29 01:24:24 -0600

    Feature: nvim-cmp was deprected in favor to blink-cmp

--- lua/moruz/lazy/cmp.lua
+++ lua/moruz/lazy/cmp.lua
@@ -1,53 +1,56 @@
-
 return {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-        "hrsh7th/cmp-buffer", -- source for text in buffer
-        "hrsh7th/cmp-path", -- source for file system paths
-        {
-            "L3MON4D3/LuaSnip",
-            version = "v2.*",
-            -- install jsregexp (optional!).
-            build = "make install_jsregexp",
-        },
-        "rafamadriz/friendly-snippets",
-        "onsails/lspkind.nvim", -- vs-code like pictograms
-    },
-    config = function()
-        local cmp = require("cmp")
-        local lspkind = require("lspkind")
-        local luasnip = require("luasnip")
-
-        require("luasnip.loaders.from_vscode").lazy_load()
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.close(),
-                ["<CR>"] = cmp.mapping.confirm({
-                    behavior = cmp.ConfirmBehavior.Replace,
-                    select = true,
-                }),
-            }),
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "luasnip" },
-                { name = "buffer" },
-                { name = "path" },
-            }),
-        })
-
-        vim.cmd([[
-      set completeopt=menuone,noinsert,noselect
-      highlight! default link CmpItemKind CmpItemMenuDefault
-    ]])
-    end,
+	-- 	"hrsh7th/nvim-cmp",
+	-- 	event = "InsertEnter",
+	-- 	dependencies = {
+	-- 		"hrsh7th/cmp-buffer", -- source for text in buffer
+	-- 		"hrsh7th/cmp-path", -- source for file system paths
+	-- 		{
+	-- 			"L3MON4D3/LuaSnip",
+	-- 			version = "v2.*",
+	-- 			-- install jsregexp (optional!).
+	-- 			build = "make install_jsregexp",
+	-- 		},
+	-- 		"rafamadriz/friendly-snippets",
+	-- 		"onsails/lspkind.nvim", -- vs-code like pictograms
+	-- 	},
+	-- 	config = function()
+	-- 		vim.opt.completeopt = { "menuone", "noselect", "noinsert" }
+	-- 		local cmp = require("cmp")
+	-- 		local lspkind = require("lspkind")
+	-- 		local luasnip = require("luasnip")
+	--
+	-- 		require("luasnip.loaders.from_vscode").lazy_load()
+	--
+	-- 		cmp.setup({
+	-- 			snippet = {
+	-- 				expand = function(args)
+	-- 					luasnip.lsp_expand(args.body)
+	-- 				end,
+	-- 			},
+	-- 			mapping = cmp.mapping.preset.insert({
+	-- 				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+	-- 				["<C-f>"] = cmp.mapping.scroll_docs(4),
+	-- 				["<C-Space>"] = cmp.mapping.complete(),
+	-- 				["<C-e>"] = cmp.mapping.close(),
+	-- 				["<CR>"] = cmp.mapping.confirm({
+	-- 					behavior = cmp.ConfirmBehavior.Replace,
+	-- 					select = true,
+	-- 				}),
+	-- 			}),
+	-- 			sources = cmp.config.sources({
+	-- 				{ name = "nvim_lsp", priority = 1000 }, -- Highest priority for smart suggestions
+	-- 				{ name = "luasnip", priority = 750 }, -- Mid priority for your snippets
+	-- 				{
+	-- 					name = "buffer",
+	-- 					priority = 500, -- Lower priority for general text
+	-- 				},
+	-- 				{ name = "path", priority = 250 }, -- Lowest priority for file paths
+	-- 			}),
+	-- 		})
+	--
+	-- 		vim.cmd([[
+	--       set completeopt=menuone,noinsert,noselect
+	--       highlight! default link CmpItemKind CmpItemMenuDefault
+	--     ]])
+	-- 	end,
 }
